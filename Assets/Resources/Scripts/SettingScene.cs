bcea444493a43fd594c7ec58e380cf71b0a57ff8
flybird <bnseobto@gmail.com>
2015-04-27 21:03:12 +0800

    add SettingScence

---
+++ Assets/Resources/Scripts/SettingScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick(){
+		Debug.Log ("onClick");
+		Application.LoadLevel(0);
+	}
+}
