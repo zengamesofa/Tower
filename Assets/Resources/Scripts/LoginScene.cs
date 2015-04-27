c81ced5b64d262294f907cc0d2a64f28925c3fc8
Arrwun <arrwun@gamesofa.com>
2015-04-27 21:51:00 +0800

    add change Scenes

---
+++ Assets/Resources/Scripts/LoginScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("test2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick(){
+		Debug.Log ("onClick");
+		Application.LoadLevel(1);
+	}
+}
