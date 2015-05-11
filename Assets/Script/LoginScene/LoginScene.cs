66e7ad7fe3b19a355ecafae6fc7d476b841c448f
flybird <bnseobto@gmail.com>
2015-05-11 20:29:12 +0800

    add partical and merge the final

---
+++ Assets/Script/LoginScene/LoginScene.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginScene : MonoBehaviour {
+
+	public GameObject earth;
+	public GameObject comet;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		earth.transform.Rotate(Vector3.forward, 1 * Time.deltaTime);
+//		comet.transform.RotateAround (new Vector3(336, -121, 0), new Vector3(10, 10, 50), Time.deltaTime * 0.5);
+	}
+
+	public void SingleClick(){
+//		Application.LoadLevel(1);
+	}
+
+	public void MultiplayerClick(){
+//		Application.LoadLevel(1);
+	}
+
+	public void SettingClick(){
+		Application.LoadLevel(1);
+	}
+
+	public void ExitClick(){
+//		Application.LoadLevel(1);
+	}
+}
