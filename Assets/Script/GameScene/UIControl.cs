6bcf62859017cb72007bfc037dde16ce7b6b819e
flybird <bnseobto@gmail.com>
2015-05-27 13:56:37 +0800

    upload this right version, previous version was crashing

---
+++ Assets/Script/GameScene/UIControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+
+	public UISlider timeSlider;
+	public UILabel peopleNumLabel;
+	public UILabel houseNumLabel;
+
+	//new Time
+	public float time;
+	public float maxTime;
+
+	//Game value
+	public float disTime;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		time = time - disTime;
+		timeSlider.sliderValue = time / maxTime;
+	}
+
+    void setPeopleNumber(int number)
+	{
+		peopleNumLabel.text = "" + number;
+	}
+
+    void setHouseNumber(int number)
+	{
+		houseNumLabel.text = "" + number;
+	}
+}
