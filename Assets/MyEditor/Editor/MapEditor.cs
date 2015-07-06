1d163301c8ed5aed42d68cf927c20cd6f13b2531
flybird <bnseobto@gmail.com>
2015-07-06 21:43:17 +0800

    add map create

--- Assets/MyEditor/Editor/MapEditor.cs
+++ Assets/MyEditor/Editor/MapEditor.cs
@@ -5,27 +5,53 @@ using System.IO;
 
 public class MapEditor : EditorWindow {
 
+	Color currentColor = Color.red;
+	string fileName = "save";
 	GameObject house;
 
 	public void OnEnable(){
 		house = (GameObject)Resources.Load("house", typeof(GameObject));
 		SceneView.onSceneGUIDelegate += OnSceneGui;
-
-		Debug.Log ("OnEnable");
+		
+		//Debug.Log ("OnEnable");
 	}
 
 	public void OnDisable()
 	{
 		//SceneView.onSceneGUIDelegate -= OnSceneGui;
-
-		Debug.Log ("OnDisable");
+		//Debug.Log ("OnDisable");
 	}
 
 	public void OnGUI(){
 		GUILayout.BeginVertical(); GUILayout.Label(" AutoModel Ver.1.0 ");
-		
 		GUILayout.BeginHorizontal(); 
 		GUILayout.Label("Create Map "); 
+		if (GUILayout.Button ("Load", GUILayout.Width (50))) {
+			//read
+			if(File.Exists(fileName)){
+				var src = File.OpenText(fileName);
+				string line = src.ReadLine();
+				while(line != null){
+					line = src.ReadLine();
+					char[] splitchar = { ',' };
+					string[] data = line.Split(splitchar);
+
+					GameObject container = GameObject.Find ("container");
+					GameObject obj = (GameObject)PrefabUtility.InstantiatePrefab(house);
+					Vector3 position = new Vector3(float.Parse(data[0]), float.Parse(data[1]), 0); 
+					obj.transform.position = position;
+					obj.transform.parent = container.transform;
+					
+					Color color = HexToColor(data[2]);
+					MyBall myball = obj.gameObject.GetComponent<MyBall>();
+					Renderer rander = myball.GetComponent<Renderer>();
+					rander.material.color = color;
+				}  
+			} else {
+				//Debug.Log("Could not Open the file: " + fileName + " for reading.");
+				return;
+			}
+		}
 		if (GUILayout.Button ("Create", GUILayout.Width (50))) {
 			//Debug.Log("Button Click");
 			for (int i=0; i<10; i++)
@@ -45,16 +71,35 @@ public class MapEditor : EditorWindow {
 		}
 		if (GUILayout.Button ("Clear", GUILayout.Width (50))) {
 			GameObject container = GameObject.Find ("container");
-			int childs = container.transform.childCount; //container.transform.childCount;
+			int childs = container.transform.childCount;
 			for (int i = childs - 1; i >= 0; i--)
 			{
 				GameObject.Destroy(container.transform.GetChild(i).gameObject);
 			}
+
+			StreamWriter sr = File.CreateText(fileName);
+			sr.WriteLine("");
+			sr.Close();
 		}
 		GUILayout.EndHorizontal();
-		
 		GUILayout.EndVertical();
 
+
+		//choose color
+		GUILayout.BeginVertical();
+		GUILayout.BeginHorizontal(); 
+		GUILayout.Label("Choose Color."); 
+		if (GUILayout.Button ("Red", GUILayout.Width (50))) {
+			currentColor = Color.red;
+		}
+		if (GUILayout.Button ("Blue", GUILayout.Width (50))) {
+			currentColor = Color.blue;
+		}
+		if (GUILayout.Button ("Green", GUILayout.Width (50))) {
+			currentColor = Color.green;
+		}
+		GUILayout.EndHorizontal();
+		GUILayout.EndVertical();
 
 		Handles.BeginGUI();
 		GUI.Button(new Rect(Screen.width-110,Screen.height-60,100,20), "Create Bomb"); 
@@ -84,13 +129,17 @@ public class MapEditor : EditorWindow {
 					GameObject obj = (GameObject)PrefabUtility.InstantiatePrefab(house);
 					obj.transform.position = mouseWorldPos;
 					obj.transform.parent = container.transform;
+
+					MyBall myball = obj.gameObject.GetComponent<MyBall>();
+					myball.ballcolor = currentColor;
+					Renderer rander = myball.GetComponent<Renderer>();
+					rander.material.color = currentColor;
 				}
 			}
 		}
 
 		if (e.type == EventType.MouseUp) 
 		{
-			string fileName = "save";
 			if (File.Exists(fileName))
 			{
 				Debug.Log(fileName + " already exists.");
@@ -100,23 +149,27 @@ public class MapEditor : EditorWindow {
 			GameObject container = GameObject.Find ("container");
 			foreach (Transform ball in container.transform)
 			{
-				//Debug.Log("ball x:" + ball.transform.position.x + " ,y" + ball.transform.position.y);
-				sr.WriteLine ("{0},{1}", ball.transform.position.x, ball.transform.position.y);
+				MyBall myball = ball.gameObject.GetComponent<MyBall>();
+				Renderer rander = myball.GetComponent<Renderer>();
+				Color color = rander.material.color;
+				//Debug.Log("ball x:" + ball.transform.position.x + " ,y" + ball.transform.position.y + " ,color:" + ColorToHex(color));
+				sr.WriteLine ("{0},{1},{2}", ball.transform.position.x, ball.transform.position.y, ColorToHex(color));
 			}
 			sr.Close();
-
-			//read
-			if(File.Exists(fileName)){
-				var src = File.OpenText(fileName);
-				var line = src.ReadLine();
-				while(line != null){
-					Debug.Log("read" + line); // prints each line of the file
-					line = src.ReadLine();
-				}  
-			} else {
-				Debug.Log("Could not Open the file: " + fileName + " for reading.");
-				return;
-			}
 		}
+	}
+
+	string ColorToHex(Color32 color)
+	{
+		string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+		return hex;
+	}
+	
+	Color HexToColor(string hex)
+	{
+		byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+		return new Color32(r,g,b, 255);
 	}
 }
